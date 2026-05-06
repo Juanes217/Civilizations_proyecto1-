2b63aeb952c154d945ab064419ec6ff32129fdbf
NilGm <nilgarciam19@gmail.com>
2026-05-06 20:07:47 +0200

    exceptions

--- M03 - Programació/Projecto_civilizations/src/Projecto/BuildingException.java
+++ M03 - Programació/Projecto_civilizations/src/Projecto/BuildingException.java
@@ -1,7 +1,15 @@
 package Projecto;
 
 public class BuildingException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    // Constructor con mensaje personalizado
     public BuildingException(String message) {
         super(message);
+    }
+
+    // Constructor por defecto
+    public BuildingException() {
+        super("Faltan edificios necesarios para construir esta unidad.");
     }
 }
--- M03 - Programació/Projecto_civilizations/src/Projecto/ResourceException.java
+++ M03 - Programació/Projecto_civilizations/src/Projecto/ResourceException.java
@@ -1,7 +1,13 @@
 package Projecto;
 
 public class ResourceException extends Exception {
+    private static final long serialVersionUID = 2L;
+
     public ResourceException(String message) {
         super(message);
+    }
+
+    public ResourceException() {
+        super("No hay recursos suficientes para completar la acción.");
     }
 }
