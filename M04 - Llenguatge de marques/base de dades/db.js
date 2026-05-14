dc1a36eba0a1c5143aa5c0f47bb91a39041af75a
Juanes217 <meraagudeloj@gmail.com>
2026-05-14 16:00:12 +0200

    conectar don la base de datos

--- M04 - Llenguatge de marques/base de dades/db.js
+++ M04 - Llenguatge de marques/base de dades/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'test123@', // Asegúrate de que esta sea tu clave real de MySQL
+    password: 'test123@', 
     database: 'juego_civilizaciones',
     waitForConnections: true,
     connectionLimit: 10,
